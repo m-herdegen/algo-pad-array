18a7e2c6ed6ec380406567a0c3b7ce310f54712d
m-herdegen <93681016+m-herdegen@users.noreply.github.com>
2022-06-01 21:08:11 -0400

    working copy

--- js/padArray.js
+++ js/padArray.js
@@ -1,4 +1,15 @@
 // REMEMBER TO PSEUDOCODE
 const pad = (array, minSize, value=null) => {
+    let padding = array.length - minSize
 
+    if (padding < 0){
+        while(padding){
+            array.push(value)
+            padding += 1
+        }
+    }
+    return array
 }
+
+console.log(pad([1,2,3], 5)) // should == [1,2,3,null,null]
+console.log(pad([1,2,3], 5, 'apple')) // should == [1,2,3,'apple', 'apple']
