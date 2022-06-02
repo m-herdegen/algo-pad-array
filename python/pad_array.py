c987f16247ad93f46f9cb52241ef2dc5412fba51
m-herdegen <93681016+m-herdegen@users.noreply.github.com>
2022-06-01 21:01:22 -0400

    working

--- python/pad_array.py
+++ python/pad_array.py
@@ -1,2 +1,12 @@
 #REMEMBER TO PSEUDOCODE
 def pad(array, min_size, value = None):
+    # find the difference between the current size and the minimum size
+    padding = len(array) - min_size
+    # if the current size is less than the minimum size
+    if padding < 0:
+        while padding:
+            # add padding to the answer array
+            array.append(value)
+            padding+=1
+    
+    return array
--- python/pad_array_spec.py
+++ python/pad_array_spec.py
@@ -1,1 +1,5 @@
 # Write unit tests!
+from pad_array import pad
+print(pad([1,2,3], 5) == [1,2,3,None,None])
+
+print(pad([1,2,3], 5, 'apple') == [1,2,3,'apple', 'apple'])
